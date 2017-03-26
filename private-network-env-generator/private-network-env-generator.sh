65c1a0ff49e8e358ed63eaa18dc3457621c85113
pratikpparikh <pratik.p.parikh@gmail.com>
2017-03-26 17:06:15 -0400

    Update private-network-env-generator.sh

--- private-network-env-generator/private-network-env-generator.sh
+++ private-network-env-generator/private-network-env-generator.sh
@@ -3,7 +3,7 @@
 set -e
 
 IFACE=${1:-eth2}
-TARGET=${2:-"/etc/private-network-environment"}
+TARGET=${2:-"/etc/ip-environment"}
 
 IP=`ifconfig $IFACE | grep -m 1 inet | awk '{print $2}'`
 
@@ -11,4 +11,4 @@ echo "Creating ${TARGET} file with private networking configuration"
 
 touch ${TARGET}
 
-echo "COREOS_REAL_PRIVATE_IPV4=${IP}" > ${TARGET}
+echo "PRIVATE_IPV4=${IP}" > ${TARGET}
