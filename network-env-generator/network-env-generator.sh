f0204e118e0ed4405781635f48c1cfa801490195
Pratik Parikh <parikhp@opentext.com>
2017-04-01 12:27:09 -0400

    updated

---
+++ network-env-generator/network-env-generator.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+IFACE=${1:-eth2}
+TARGET=${2:-"/etc/network-environment"}
+
+IP=`ifconfig $IFACE | grep -m 1 inet | awk '{print $2}'`
+
+echo "Creating ${TARGET} file with private networking configuration"
+
+touch ${TARGET}
+
+echo "PRIVATE_IPV4=${IP}" > ${TARGET}
+
+source /etc/network-environment
